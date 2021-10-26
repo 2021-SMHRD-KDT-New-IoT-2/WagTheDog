237895f0863d4ff73458410a864d2aba926ffaff
boriyoondal <boriyoondal@naver.com>
2021-10-26 15:11:06 +0900

    commit!

---
+++ Project01/src/com/model/MemberVO.java
@@ -0,0 +1,14 @@
+package com.model;
+
+public class MemberVO {
+	private String name;
+	private String id;
+	private String address;
+	
+	//»ý¼ºÀÚ
+	public MemberVO(String name, String id, String address) {
+		this.name = name;
+		this.id = id;
+		this.address = address;	
+	}
+}
